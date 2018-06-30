e724e717605fc1e99112371f4cd8199e3f55608f
Adrian Schmidt <adrian@stealmycode.se>
2018-06-30 13:53:18 +0200

    chore(dev-env): add linter
    Add tslint and prettier. Update files to pass tests. Add pre-commit hook.

--- src/components/app-home/app-home.tsx
+++ src/components/app-home/app-home.tsx
@@ -1,28 +1,25 @@
-import { Component } from '@stencil/core';
-
+import { Component } from "@stencil/core";
 
 @Component({
-  tag: 'app-home',
-  styleUrl: 'app-home.css'
+    styleUrl: "app-home.css",
+    tag: "app-home",
 })
 export class AppHome {
+    public render() {
+        return (
+            <div class="app-home">
+                <p>
+                    Welcome to the Stencil App Starter. You can use this starter
+                    to build entire apps all with web components using Stencil!
+                    Check out our docs on{" "}
+                    <a href="https://stenciljs.com">stenciljs.com</a> to get
+                    started.
+                </p>
 
-  render() {
-    return (
-      <div class='app-home'>
-        <p>
-          Welcome to the Stencil App Starter.
-          You can use this starter to build entire apps all with
-          web components using Stencil!
-          Check out our docs on <a href='https://stenciljs.com'>stenciljs.com</a> to get started.
-        </p>
-
-        <stencil-route-link url='/profile/stencil'>
-          <button>
-            Profile page
-          </button>
-        </stencil-route-link>
-      </div>
-    );
-  }
+                <stencil-route-link url="/profile/stencil">
+                    <button>Profile page</button>
+                </stencil-route-link>
+            </div>
+        );
+    }
 }
