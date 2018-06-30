c2e92f8c932f495104aeaa7153b0893d6ac13890
Adrian Schmidt <adrian@stealmycode.se>
2018-06-30 13:53:18 +0200

    chore(dev-env): add linter
    Add tslint and prettier. Update files to pass tests. Add pre-commit hook.

--- src/components/app-profile/app-profile.tsx
+++ src/components/app-profile/app-profile.tsx
@@ -1,25 +1,23 @@
-import { Component, Prop } from '@stencil/core';
-import { MatchResults } from '@stencil/router';
-
+import { Component, Prop } from "@stencil/core";
+import { MatchResults } from "@stencil/router";
 
 @Component({
-  tag: 'app-profile',
-  styleUrl: 'app-profile.css'
+    styleUrl: "app-profile.css",
+    tag: "app-profile",
 })
 export class AppProfile {
-  
-  @Prop() match: MatchResults;
+    @Prop() public match: MatchResults;
 
-  render() {
-    if (this.match && this.match.params.name) {
-      return (
-        <div class='app-profile'>
-          <p>
-            Hello! My name is {this.match.params.name}.
-            My name was passed in through a route param!
-          </p>
-        </div>
-      );
+    public render() {
+        if (this.match && this.match.params.name) {
+            return (
+                <div class="app-profile">
+                    <p>
+                        Hello! My name is {this.match.params.name}. My name was
+                        passed in through a route param!
+                    </p>
+                </div>
+            );
+        }
     }
-  }
 }
