0d9d2daf6c0fa8601d760cb3b2f03e8bdc98a816
Adrian Schmidt <adrian@stealmycode.se>
2018-07-01 00:15:09 +0200

    chore: clean build

---
+++ docs/sw.js
@@ -0,0 +1,72 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.3.0/workbox-sw.js");
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "assets/icon/favicon.ico",
+    "revision": "d2f619d796fbe8bed6200da2691aa5b6"
+  },
+  {
+    "url": "assets/icon/icon.png",
+    "revision": "b96ad6e1e0b755c8cd45e6aec40bca25"
+  },
+  {
+    "url": "build/app.css",
+    "revision": "0c5dad92482d9a7c7c253510f5082465"
+  },
+  {
+    "url": "build/app.js",
+    "revision": "dfa7b9da2f1e9722e6cc0552d3597dc5"
+  },
+  {
+    "url": "build/app/app.eeqcbigw.js",
+    "revision": "04dfbd38f3ff4f9444deda54681fef41"
+  },
+  {
+    "url": "build/app/app.iqjpnrrj.js",
+    "revision": "12eed75e3519e4955ea666c2f7f0ef7b"
+  },
+  {
+    "url": "build/app/cz9tggv8.es5.js",
+    "revision": "fb7532b28decf8557fb0b51f20b322c9"
+  },
+  {
+    "url": "build/app/cz9tggv8.js",
+    "revision": "c015990912a37d15a2eb6ce118350959"
+  },
+  {
+    "url": "build/app/loettoy3.es5.js",
+    "revision": "341012f0e5919e03a63ef7878f559096"
+  },
+  {
+    "url": "build/app/loettoy3.js",
+    "revision": "b9c0b704c6a7571852e1884c41388781"
+  },
+  {
+    "url": "index.html",
+    "revision": "5d93a4005f148dd1a444cddb9e8d097e"
+  },
+  {
+    "url": "manifest.json",
+    "revision": "b2610260ffcedd21691ce9dc79285100"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
