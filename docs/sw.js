67c2357477ce81f6dbc84e858d89bb1ae6f10ec3
Adrian Schmidt <adrian@stealmycode.se>
2018-07-01 08:45:32 +0200

    chore: re-add missing build output

---
+++ docs/sw.js
@@ -0,0 +1,64 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.3.0/workbox-sw.js");
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "assets/icon/favicon.ico",
+    "revision": "d2f619d796fbe8bed6200da2691aa5b6"
+  },
+  {
+    "url": "assets/icon/icon.png",
+    "revision": "b96ad6e1e0b755c8cd45e6aec40bca25"
+  },
+  {
+    "url": "build/app.css",
+    "revision": "0c5dad92482d9a7c7c253510f5082465"
+  },
+  {
+    "url": "build/app.js",
+    "revision": "ae2992fd3d1d90bbe1804d4d1a8d3742"
+  },
+  {
+    "url": "build/app/app.eeqcbigw.js",
+    "revision": "04dfbd38f3ff4f9444deda54681fef41"
+  },
+  {
+    "url": "build/app/app.iqjpnrrj.js",
+    "revision": "12eed75e3519e4955ea666c2f7f0ef7b"
+  },
+  {
+    "url": "build/app/g0l46j6o.es5.js",
+    "revision": "d10ab62305cebb9932e6d5891b0a4c2c"
+  },
+  {
+    "url": "build/app/g0l46j6o.js",
+    "revision": "edfb2df49308966ceada4395a1546eba"
+  },
+  {
+    "url": "index.html",
+    "revision": "255b257fb8690b8af5d9e4e7a1d733f2"
+  },
+  {
+    "url": "manifest.json",
+    "revision": "b2610260ffcedd21691ce9dc79285100"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
