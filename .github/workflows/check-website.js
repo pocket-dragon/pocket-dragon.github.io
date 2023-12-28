9d80f9298dcde0c16b0605e89a5e695fd78573f0
Adrian Schmidt <adrian.schmidt@lime.tech>
2023-12-28 12:52:43 +0100

    ci(up-check.yml): split website check into separate file
    The script was trying to run javascript straight in the bash shell before 🤦‍♂️

---
+++ .github/workflows/check-website.js
@@ -0,0 +1,23 @@
+// check_website.js
+const puppeteer = require('puppeteer');
+
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://pocket-dragon.github.io/');
+
+    const isContentPresent = await page.evaluate(() => {
+        const header = document.querySelector('header');
+        return header && header.textContent.includes('Pocket Dragon');
+    });
+
+    await browser.close();
+
+    if (!isContentPresent) {
+        console.error('Expected content not found on the page.');
+        process.exit(1);
+    }
+})().catch(error => {
+    console.error(error);
+    process.exit(1);
+});
